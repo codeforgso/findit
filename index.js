fea57dcec04c7a4f907e9509291a4e6a82ad6188
Brendan Younger <github@brendanyounger.com>
2015-09-16 20:43:11 -0400

    Added bootstrap

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 import React from 'react';
 
+require('./less/bootstrap.less');
+
 class Root extends React.Component {
   render() {
     return (
-      <h1>Hello, world!</h1>
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-12">
+            <h1>Hello, world!</h1>
+          </div>
+        </div>
+      </div>
     );
   }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 var webpack = require('webpack');
 var path = require('path');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
   entry: [
@@ -15,12 +16,26 @@ module.exports = {
     extensions: ['', '.js', '.jsx']
   },
   module: {
-    loaders: [{
-      test: /\.jsx?$/,
+    loaders: [
+    { test: /\.jsx?$/,
       exclude: /(node_modules|bower_components)/,
       loaders: ['react-hot', 'babel'],
     },
-    { test: /\.json$/, loader: 'json' }]
+    { test: /\.json$/, loader: 'json' },
+    { test: /\.(png|jpg|jpeg|gif|svg(\?v=[0-9]\.[0-9]\.[0-9])?)$/,
+      loaders: [
+        'url?name=assets/[hash].[ext]&limit=8192',
+        'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false'
+      ]
+    },
+    { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+      loader: "url?name=assets/[hash].[ext]&limit=10000&mimetype=application/font-woff"
+    },
+    { test: /\.(ttf|eot|otf)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'file?name=assets/[hash].[ext]' },
+    { test: /\.(md|markdown)$/, loader: 'html!markdown-highlight' },
+    { test: /\.html$/, loader: 'html?minimize=false' },
+    { test: /\.less$/, loader: ExtractTextPlugin.extract('style', 'css!less') },
+    { test: /\.css$/, loader: ExtractTextPlugin.extract('style', 'css') } ]
   },
   devServer: {
     noInfo: true,
@@ -29,6 +44,7 @@ module.exports = {
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin()
+    new webpack.NoErrorsPlugin(),
+    new ExtractTextPlugin("styles.css")
   ]
 };
