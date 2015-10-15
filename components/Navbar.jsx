f73f37c89f45392a09c59b5038e9a54a531ce2ae
Brendan Younger <github@brendanyounger.com>
2015-10-14 20:15:46 -0400

    Add about modal dialog

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,9 +1,59 @@
 import React from 'react'
-import {Navbar, NavBrand, Nav, NavItem, NavDropdown, MenuItem} from 'react-bootstrap'
+import {Navbar, NavBrand, Nav, NavItem, NavDropdown, MenuItem, Modal, Button, OverlayTrigger} from 'react-bootstrap'
 
 let Logo = require('../images/logo.png');
+let BuiltInLogo = require('../images/gsobuilt.jpg');
+
+class About extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {showModal:false}
+  }
+
+  render() {
+    return (
+      <Nav pullRight={true} {...this.props}>
+        <NavItem eventKey={1} href="#" onClick={(e) => this.setState({showModal:true})}>About</NavItem>
+
+        <Modal show={this.state.showModal} onHide={() => this.setState({showModal:false})}>
+          <Modal.Header closeButton>
+            <Modal.Title>About Greensboro Finda Facility</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <p>Finda is a generic "find-a" app for geographic datasets.</p>
+            <p><strong>Please send feedback, ideas, and bug reports to our <a href="https://github.com/codeforgreensboro/finda/issues">Github page</a>.</strong></p>
+
+            <hr />
+
+            <h4>About Code for Greensboro</h4>
+            <p>We're developers, designers, data nerds, politically-minded people with ideas for making things better, citizens, users of software—in other words, we are this community. We want to use these talents to make Greensboro's government work with the web in new and better ways.</p>
+
+            <p>Feel free to stop by one of our <a href="https://www.bit.ly/cfgrsvp">Bi-weekly Hack Nights</a>, <a href="https://www.github.com/codeforgso">check us out on Github</a>, or <a href="mailto:hello@codeforgreensboro.org">simply drop us a line</a>!</p>
+
+            <hr />
+
+            <h4>Credit Where Due</h4>
+
+            <p>Finda was originally built by <a href="https://github.com/codeforboston">Code for Boston</a>. We stand on the shoulders of giants!</p>
+
+          </Modal.Body>
+          <Modal.Footer>
+            <a href="http://www.gsobuilt.co" target="_blank">
+              <img alt="Built in GSO logo" className="pull-left" src={BuiltInLogo} style={{height:'2.5em'}} />
+            </a>
+            <Button onClick={() => this.setState({showModal:false})}>Close</Button>
+          </Modal.Footer>
+        </Modal>
+      </Nav>
+    );
+  }
+}
 
 export default class extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
   render() {
     return (
       <Navbar staticTop={true} fluid={true}>
@@ -21,12 +71,12 @@ export default class extends React.Component {
             <div className="form-group">
               <input type="text" className="form-control" placeholder="Search" />
             </div>
-            <button type="submit" className="btn btn-default">Submit</button>
+            {' '}
+            <button type="submit" className="btn btn-primary">Submit</button>
           </form>
         </Nav>
-        <Nav pullRight={true}>
-          <NavItem eventKey={1} href="#">About</NavItem>
-        </Nav>
+
+        <About />
       </Navbar>
     );
   }
